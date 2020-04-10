0d0324eb14623323afe09ff7b84186802c071038
Itamar <it.segman@gmail.com>
2020-04-10 16:04:40 +0300

    Listings.
    Add Listings pages to the App.

--- btre/urls.py
+++ btre/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = {
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
-]
+}
